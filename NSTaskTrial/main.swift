af706142efa4ef0ac21a0a0fd622510fd61266f9
sanooj <sanojneo@yahoo.co.in>
2019-04-24 21:48:46 +0100

    Initial Commit

---
+++ NSTaskTrial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NSTaskTrial
+//
+//  Created by carvak on 24/04/2019.
+//  Copyright © 2019 sanooj. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
